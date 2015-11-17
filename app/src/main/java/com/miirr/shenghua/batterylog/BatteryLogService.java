1a2cc9e5809b77dd646fc2e5d0b32f1f43b1f0ac
Shenghua Su <shenghua.su@gmail.com>
2015-11-17 21:19:37 +0800

    add battery receiver to service

--- app/src/main/java/com/miirr/shenghua/batterylog/BatteryLogService.java
+++ app/src/main/java/com/miirr/shenghua/batterylog/BatteryLogService.java
@@ -1,8 +1,12 @@
 package com.miirr.shenghua.batterylog;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.AsyncTask;
+import android.os.BatteryManager;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -14,19 +18,37 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import javax.net.ssl.HttpsURLConnection;
-
 /**
  * Created by shenghua on 11/16/15.
  */
 public class BatteryLogService extends Service {
-    private static final String TAG = "BatteryLogService";
+
+    // tag
+    private static final String TAG = BatteryLogService.class.getSimpleName();
+
+    // broadcast
+    public static final String ACTION_BATTERYSTATUS_CHANGED = "com.miirr.shenghua.batteryLog.CHANGED";
+    private static BroadcastReceiver mBatteryReceiver;
+
+    // charge type
+    private static final int BATTERY_UNDEFINED_CHARGESTATUS = -1;
+    public static final int BATTERY_NO_CHARGE = 0;
+    public static final int BATTERY_AC_CHARGE = 1;
+    public static final int BATTERY_USB_CHARGE = 2;
+    private static int mChargeType = BATTERY_UNDEFINED_CHARGESTATUS;
+
+    // power
+    private static final int BATTERY_UNKNOWN_POWER = -1;
+    private static int mPluginPower = BATTERY_UNKNOWN_POWER;
+    private static int mPlugoutPower = BATTERY_UNKNOWN_POWER;
+    private static long mPluginTime = 0;
+    private static long mPlugoutTime = 0;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
         if (intent != null && intent.hasExtra(BATTERY_SERVICE)) {
-            new PostBatteryLogAsync().execute();
+            new SaveBatteryChargeCycleAsync().execute();
         }
         return START_STICKY;
     }
@@ -36,21 +58,118 @@ public class BatteryLogService extends Service {
         return null;
     }
 
-    private class PostBatteryLogAsync extends AsyncTask<Void, Void, Void> {
+    @Override
+    public void onCreate() {
+        Log.d(TAG, "onCreate()");
+
+        if (null == mBatteryReceiver) {
+            mBatteryReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    onNativeBatteryChanged(intent);
+                }
+            };
+            IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+            Intent intent = registerReceiver(mBatteryReceiver, filter);
+            onNativeBatteryChanged(intent);
+        }
+    }
+
+    public void onDestory() {
+        Log.d(TAG, "onDestory()");
+        unregisterReceiver(mBatteryReceiver);
+        mBatteryReceiver = null;
+        super.onDestroy();
+    }
+
+    private void recordPlugin() {
+        mPluginTime = System.currentTimeMillis();
+    }
+
+    private void recordPlugout() {
+        mPlugoutTime = System.currentTimeMillis();
+    }
+
+    private void onNativeBatteryChanged(Intent intent) {
+
+        if (intent.getAction().equals(Intent.ACTION_BATTERY_CHANGED)) {
+
+            int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+            boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                    status == BatteryManager.BATTERY_STATUS_FULL;
+
+            int chargeType = BATTERY_UNDEFINED_CHARGESTATUS;
+            if (isCharging) {
+                int chargePlug = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+                if (chargePlug == BatteryManager.BATTERY_PLUGGED_USB) {
+                    chargeType = BATTERY_USB_CHARGE;
+                } else if (chargePlug == BatteryManager.BATTERY_PLUGGED_AC) {
+                    chargeType = BATTERY_AC_CHARGE;
+                }
+            } else {
+                chargeType = BATTERY_NO_CHARGE;
+            } // till here the chargeType is determined instead of BATTERY_UNDEFINED_CHARGESTATUS
+
+            if (chargeType != mChargeType) {
+                switch (mChargeType) {
+                    case BATTERY_UNDEFINED_CHARGESTATUS: // first time of launch this service
+                        if (chargeType == BATTERY_USB_CHARGE || chargeType == BATTERY_AC_CHARGE) {
+                            // assume the launch as the plugin
+                            mPluginTime = System.currentTimeMillis();
+                            mPluginPower = calculateBatteryLevel(intent);
+                        }
+                        else { // no power supply
+                            // this service launched when having power supply plugged in
+                            // as plugin power unknown, meaningless to record the cycle
+                        }
+                        break;
+
+                    case BATTERY_NO_CHARGE: // chargeType can only be either "usb or ac charge"
+                        mPluginTime = System.currentTimeMillis();
+                        mPluginPower = calculateBatteryLevel(intent);
+                        break;
+
+                    case BATTERY_USB_CHARGE:
+                    case BATTERY_AC_CHARGE: // chargeType can only be "no charge", plugin => plugout
+                        mPlugoutTime = System.currentTimeMillis();
+                        mPlugoutPower = calculateBatteryLevel(intent);
+                        if (mPlugoutPower != mPlugoutPower) { // record this charge cycle
+                        }
+                        // clear charge flag
+                        break;
+                }
+                mChargeType = chargeType;
+                broadcastActionBatterystatusChanged();
+            }
+        }
+    }
+
+    private int calculateBatteryLevel(Intent intent) {
+        int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        return ((int) ((level / (float) scale) * 100));
+    }
+
+    private void broadcastActionBatterystatusChanged() {
+        Intent i = new Intent(ACTION_BATTERYSTATUS_CHANGED);
+        sendBroadcast(i);
+    }
+
+    private class SaveBatteryChargeCycleAsync extends AsyncTask<Void, Void, Void> {
 
         @Override
         protected Void doInBackground(Void... params) {
-            postTest();
+            saveChargeCycleData();
             return null;
         }
 
 //        @Override
-        protected void onPostExecute(String length) {
+        protected void onPostExecute() {
             BatteryLogService.this.stopSelf();
         }
     }
 
-    private void postTest() {
+    private void saveChargeCycleData() {
         Log.d("BatteryPost", "postTest()");
         try {
             URL url = new URL("http://192.168.0.150/battery/app/frontend/web/");
