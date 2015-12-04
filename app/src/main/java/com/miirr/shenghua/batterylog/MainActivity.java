5dc5fcd180a40179f572c0fa6a60af051e45f394
Shenghua Su <shenghua.su@gmail.com>
2015-12-04 22:17:04 +0800

    add login

--- app/src/main/java/com/miirr/shenghua/batterylog/MainActivity.java
+++ app/src/main/java/com/miirr/shenghua/batterylog/MainActivity.java
@@ -116,7 +116,12 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_showLog) {
+        if (id == R.id.action_login) {
+
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+
+
             return true;
         }
 
