1dbdaad6e13a9e6459bfa71eeab3b5266e22290c
Shenghua Su <shenghua.su@gmail.com>
2015-12-23 18:09:32 +0800

    try to use web view(fusioncharts js) to display log, but it drains battery too fast

--- app/src/main/java/com/shenghua/battery/LogFragment.java
+++ app/src/main/java/com/shenghua/battery/LogFragment.java
@@ -1,18 +1,24 @@
 package com.shenghua.battery;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 
 /**
  * Created by shenghua on 11/14/15.
  */
 public class LogFragment extends Fragment {
+
+    private WebView mWebView;
+    private Button mButton;
 
     public static LogFragment newInstance() {
         LogFragment fragment = new LogFragment();
@@ -23,12 +29,66 @@ public class LogFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
+
         View rootView = inflater.inflate(R.layout.fragment_log, container, false);
-        WebView webView = (WebView) rootView.findViewById(R.id.webView);
-        WebSettings webSettings = webView.getSettings();
+
+        mWebView = (WebView) rootView.findViewById(R.id.webView);
+
+        WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("http://192.168.0.150/battery/app/frontend/web");
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setSupportZoom(true);
+
+        mWebView.setWebViewClient(new WebViewClient());
+
+        class WebInterface {
+            Context mContext;
+
+            WebInterface(Context c) {
+                mContext = c;
+            }
+
+            byte[] getData() {
+                return String.valueOf("{type: 'column2d',"+
+                         "renderAt: 'chart-container',"+
+                         "dataFormat: 'json',"+
+                          "dataSource: {"+
+                          "chart: {"+
+                                    "caption: \"Quarterly sales summary\","+
+                                        "numberPrefix: \"$\","+
+                                        "decimals: \"2\","+
+                                        "forceDecimals: \"1\""+
+                            "},"+
+                            "data: ["+
+                            "{ label: \"Q1\", value: \"213345\"},"+
+                            "{ label: \"Q2\", value: \"192672\"},"+
+                            "{ label: \"Q3\", value: \"201238\"},"+
+                            "{ label: \"Q4\", value: \"209881\"},"+
+                            "]"+
+                        "};").getBytes();
+            }
+        }
+
+        mWebView.addJavascriptInterface(new WebInterface(getContext()), "Android");
+        mWebView.loadUrl("file:///android_asset/batterylog.html");
+//        mWebView.loadUrl("http://192.168.0.150/battery/app/frontend/web");
+
+//        mButton = (Button) rootView.findViewById(R.id.test);
+//        mButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d("fuck--->", "bad");
+//                mWebView.loadUrl("javascript:renderBatteryLog()");
+//            }
+//        });
+
         return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+//        Log.d("fuck", "bad");
+//        mWebView.loadUrl("javascript:renderBatteryLog()");
     }
 }
