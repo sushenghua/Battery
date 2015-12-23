f9e335c2fd2b704fb281987d2a8112512865c7c7
Shenghua Su <shenghua.su@gmail.com>
2015-12-23 20:07:17 +0800

    clean code

--- app/src/main/java/com/shenghua/battery/LogFragment.java
+++ app/src/main/java/com/shenghua/battery/LogFragment.java
@@ -26,7 +26,6 @@ import java.util.Map;
 public class LogFragment extends Fragment {
 
     private WebView mWebView;
-    private Button mButton;
 
     private static final String URL = "http://192.168.0.150/battery/app/frontend/web";
 
@@ -49,32 +48,9 @@ public class LogFragment extends Fragment {
         webSettings.setBuiltInZoomControls(true);
         webSettings.setSupportZoom(true);
 
-
-//        CookieManager cookieManager = CookieManager.getInstance();
-//        cookieManager.setAcceptCookie(true);
-//        String cookieString = getCookies(getContext());
-//        Log.d("----->", cookieString);
-//        cookieManager.setCookie(URL, cookieString);
-//        cookieManager.removeAllCookie();
-//        CookieSyncManager.createInstance(getContext()).sync();
-//        cookieManager.removeSessionCookie();
-//        CookieSyncManager.getInstance().sync();
-
-
         mWebView.setWebViewClient(new WebViewClient());
-//        mWebView.loadUrl(URL);
-
         Map<String, String> headers = new HashMap<String, String>();
-        String cookieValue = getCookies(getContext());
-        //Log.d("----->", cookieValue);
-        headers.put("Cookie", cookieValue);
-//        mWebView.setWebViewClient(new WebViewClient() {
-//            @Override
-//            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-//                view.loadUrl(url, headers);
-//                return false;
-//            }
-//        });
+        headers.put("Cookie", getCookies(getContext()));
         mWebView.loadUrl(URL, headers);
 
         return rootView;
@@ -82,8 +58,7 @@ public class LogFragment extends Fragment {
 
     private String getCookies(Context context) {
 
-        String mergedCookie = "";//"7eeb1890096146dc392b6a2443b0f6475c182de66a05cad95d1062de877488b2a%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_csrf%22%3Bi%3A1%3Bs%3A32%3A%22rf92s_paqTDULaEwe-S1hChZhZDZE924%22%3B%7D";
-        mergedCookie = PrefsStorageDelegate.getStringValue("CsrfCookes");
+        String mergedCookie = PrefsStorageDelegate.getStringValue("CsrfCookes");
 
         String sessionCookie = PrefsStorageDelegate.getStringValue("SessionCookie");
         if (sessionCookie.length() > 0) {
