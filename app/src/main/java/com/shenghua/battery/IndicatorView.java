8192f74ff0b0b440d0b78a4e3ccbc7732797c14f
Shenghua Su <shenghua.su@gmail.com>
2016-01-03 13:46:43 +0800

    clean code

--- app/src/main/java/com/shenghua/battery/IndicatorView.java
+++ app/src/main/java/com/shenghua/battery/IndicatorView.java
@@ -17,32 +17,11 @@ class IndicatorView extends ImageView {
     public static final int INDICATOR_WARNING = 5;
     public static final int INDICATOR_INACTIVE = 6;
 
-    private static final int[] INDICATOR_STATE = {
-            R.attr.indicator_value
-//            R.attr.indicator_support,
-//            R.attr.indicator_good,
-//            R.attr.indicator_notgood,
-//            R.attr.indicator_warning,
-//            R.attr.indicator_inactive,
-//            R.attr.indicator_unsupport
-    };
-
     private int indicatorValue = 0;
 
     public IndicatorView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
-
-//    @Override
-//    public int[] onCreateDrawableState(int extraSpace) {
-//        final int[] drawableState = super.onCreateDrawableState(extraSpace + 1);
-//        if (indicatorValue >= INDICATOR_UNSUPPORT && indicatorValue <= INDICATOR_INACTIVE) {
-//            mergeDrawableStates(drawableState, INDICATOR_STATE);
-//        }
-//        return drawableState;
-//
-////        return INDICATOR_STATE;
-//    }
 
     public void setIndicatorValue(int value) {
 
